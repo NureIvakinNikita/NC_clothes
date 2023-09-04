dfaa482b3046d4b5b8aa7ed77b7f9839f12053b2
Ивакин Никита <nikita.ivakin@nure.ua>
2023-09-04 18:07:54 +0300

    Start of project

---
+++ src/main/java/com/example/nc_clothes_1/NcClothes1Application.java
@@ -0,0 +1,13 @@
+package com.example.nc_clothes_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcClothes1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcClothes1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nc_clothes_1/NcClothes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nc_clothes_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcClothes1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
