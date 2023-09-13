e161a9ab4f6ee3134baf5cd0369b3cd26c4f64c3
Ивакин Никита <nikita.ivakin@nure.ua>
2023-09-13 18:59:52 +0300

    added the db connection and user tabel
    it's a test table just for creating the auth logic

--- src/main/java/com/example/nc_clothes_1/user/User.java
+++ src/main/java/com/example/nc_clothes_1/user/User.java
@@ -1,11 +1,34 @@
 package com.example.nc_clothes_1.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "password")
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
     private Role role;
 }
---
+++ src/main/resources/db/changelog/001-create-user-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    email VARCHAR,
+    password VARCHAR,
+    role VARCHAR
+)
